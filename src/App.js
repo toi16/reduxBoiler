d15084ee1839ca4927403ec54be6de6492ced2d0
Matthew Howard <mhoward865@gmail.com>
2018-04-19 19:38:56 +0100

    create test form

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import Form from './components/Form';
 
 class App extends Component {
     render() {
         return (
             <Provider store={store}>
                 <View>
-                    <Text>
-                        Hello!
-                    </Text>
+                    <Form />
                 </View>
             </Provider>
         );
